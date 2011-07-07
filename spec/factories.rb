a813b443bdd870458fac045cf1cd7a11d141017d
Kimmo Heikkinen <kimmo.heikkinen@eficode.fi>
2011-07-07 10:42:26 +0300

    User model with passwords and show page. Time for a cigarette break

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name     "Kimmo Heikkinen"
+  user.email    "kimmo.heikkinen@eficode.fi"
+  user.password "foojabar"
+  user.password_confirmation  "foojabar"
+end
