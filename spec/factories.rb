0f27cbafe79391bed0e9f40b56902d8d0d3bcc5f
Kimmo Heikkinen <kimmo.heikkinen@eficode.fi>
2011-07-11 13:56:16 +0300

    User microposts done

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,3 +8,8 @@ end
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
 end
+
+Factory.define :micropost do |micropost|
+  micropost.content "BLAH BLAH BLAH"
+  micropost.association :user
+end
